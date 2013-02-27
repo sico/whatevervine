eb0a8801690ce67636a99b01a6dfdd5c0d74c299
John Sico <jsico@gilt.com>
2013-02-27 11:01:44 -0500

    init

--- tweather.rb
+++ tweather.rb
@@ -7,12 +7,23 @@ class Tweather < Sinatra::Application
 
   get '/' do
     @name = fetch_tweets
-    erb :index, :locals => {:name => @name}
+    @snows = check_snow(@name)
+    erb :index, :locals => {:name => @name, :snows => @snows}
   end
 
   def fetch_tweets
-    content = open("http://search.twitter.com/search.json?geocode=40.660358599999995,-73.98967739999999,1km&q=weather").read
+    content = open("http://search.twitter.com/search.json?geocode=40.660358599999995,-73.98967739999999,5mi&q=snow&rpp=100&result_type=recent").read
     return @parsed = JSON.parse(content)
+  end
+
+  def check_snow(tweets)
+    s = 0
+    tweets['results'].each do |tweet|
+      if tweet['text'].include? "brooklyn"
+        s += 1
+      end
+    end
+    return s
   end
 
 end
